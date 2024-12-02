c8de94a72a3fa7afb2f2f215912895ccc6b0da30
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2024-12-02 15:13:13 +0100

    day 2 part 2

--- crates/day2/src/main.rs
+++ crates/day2/src/main.rs
@@ -24,6 +24,16 @@ fn part1(input: &str) -> usize {
     })
 }
 
+fn check(row: &Vec<usize>) -> bool {
+    let mut diffs: Vec<isize> = vec![];
+    let mut iter = row.iter().peekable();
+    while let Some((curr, next)) = iter.next().zip(iter.peek()) {
+        diffs.push(*curr as isize - **next as isize);
+    }
+    diffs.iter().all(|diff| (1..=3).contains(diff))
+        || diffs.iter().all(|diff| (-3..=-1).contains(diff))
+}
+
 fn part2(input: &str) -> usize {
     let input = input
         .lines()
@@ -35,23 +45,18 @@ fn part2(input: &str) -> usize {
         })
         .collect::<Vec<_>>();
     input.iter().fold(0, |acc, row| {
-        let mut has_violation = false;
-        let mut iter = row.iter();
-        let ascending = iter.next().unwrap() < iter.next().unwrap();
-        let mut iter = row.iter().peekable();
-        while let Some((curr, next)) = iter.next().zip(iter.peek()) {
-            if (ascending && curr >= next)
-                || (!ascending && curr <= next)
-                || curr.abs_diff(**next) > 3
-            {
-                if has_violation {
-                    return acc;
-                } else {
-                    has_violation = true;
+        if check(row) {
+            acc + 1
+        } else {
+            for i in 0..row.len() {
+                let mut row = row.clone();
+                row.remove(i);
+                if check(&row) {
+                    return acc + 1;
                 }
             }
+            acc
         }
-        acc + 1
     })
 }
 
@@ -59,5 +64,5 @@ fn main() {
     assert_eq!(part1(include_str!("./test")), 2);
     assert_eq!(part1(include_str!("./input")), 369);
     assert_eq!(part2(include_str!("./test")), 4);
-    // assert_eq!(part2(include_str!("./input")), 0);
+    assert_eq!(part2(include_str!("./input")), 428);
 }
