1ad9963f11933e9dbe75c177ad053d9cd4fdc185
Christoffer Hildebrandt <christoffer.hildebrandt@gmail.com>
2024-12-03 08:55:35 +0100

    day3 part2

--- crates/day3/src/main.rs
+++ crates/day3/src/main.rs
@@ -19,11 +19,8 @@ fn part1(input: &str) -> usize {
 fn part2(input: &str) -> usize {
     let dos = regex::Regex::new(r"do\(\)").unwrap();
     let donts = regex::Regex::new(r"don\'t\(\)").unwrap();
-    let mut instructions = dos
-        .find_iter(input)
-        .zip(donts.find_iter(input))
-        .flat_map(|(a, b)| vec![a, b])
-        .collect::<Vec<_>>();
+    let mut instructions = dos.find_iter(input).collect::<Vec<_>>();
+    instructions.extend(donts.find_iter(input));
     instructions.sort_by(|a, b| a.start().cmp(&b.start()));
     let rgx = regex::Regex::new(r"mul\(\d+,\d+\)").unwrap();
     rgx.find_iter(input)
@@ -54,5 +51,5 @@ fn main() {
     assert_eq!(part1(include_str!("./test")), 161);
     assert_eq!(part1(include_str!("./input")), 175015740);
     assert_eq!(part2(include_str!("./test2")), 48);
-    assert_eq!(part2(include_str!("./input")), 0);
+    assert_eq!(part2(include_str!("./input")), 112272912);
 }
