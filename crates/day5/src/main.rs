faf4bd96954922b5199c0cdbe189165cd5d0eedf
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2024-12-05 09:55:39 +0100

    day5 part1

--- crates/day5/src/main.rs
+++ crates/day5/src/main.rs
@@ -1,7 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::HashSet;
 
 fn part1(input: &str) -> usize {
-    let (ordering_rules, updates) = input.split_once("\r\n\r\n").unwrap();
+    let (ordering_rules, updates) = input.split_once("\n\n").unwrap();
     let ordering_rules = ordering_rules
         .lines()
         .map(|line| {
@@ -21,29 +21,22 @@ fn part1(input: &str) -> usize {
     let valid_updates = updates
         .iter()
         .filter(|page_nums| {
-            let mut visited = HashSet::new();
+            let mut set = HashSet::<usize>::new();
             page_nums.iter().all(|page_num| {
-                if visited.contains(page_num) {}
-                if ordering_rules
-                    .iter()
-                    .filter(|rule| visited.contains(page_num) && rule.0 == *page_num)
-                    .any(|rule| rule.1 > *page_num)
-                {
-                    println!("Invalid page number: {}", page_num);
-                    println!("{:?}", visited);
-                    return false;
+                if set.iter().any(|visited| visited == page_num) {
+                    false
+                } else {
+                    set.extend(
+                        ordering_rules
+                            .iter()
+                            .filter(|rule| rule.1 == *page_num)
+                            .map(|rule| rule.0),
+                    );
+                    true
                 }
-                visited.extend(
-                    ordering_rules
-                        .iter()
-                        .filter(|rule| rule.0 == *page_num)
-                        .map(|rule| rule.0),
-                );
-                true
             })
         })
         .collect::<Vec<_>>();
-    println!("{:?}", valid_updates);
     valid_updates.iter().fold(0, |acc, instruction| {
         acc + instruction[(instruction.len() as f32 / 2.0).floor() as usize]
     })
@@ -55,7 +48,7 @@ fn part2(input: &str) -> usize {
 
 fn main() {
     assert_eq!(part1(include_str!("./test")), 143);
-    // assert_eq!(part1(include_str!("./input")), 0);
+    assert_eq!(part1(include_str!("./input")), 6260);
     // assert_eq!(part2(include_str!("./test")), 0);
     // assert_eq!(part2(include_str!("./input")), 0);
 }
