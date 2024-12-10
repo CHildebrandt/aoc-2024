c4280b33630ed5475b3dda41b700183a281fea8c
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2024-12-10 11:07:30 +0100

    refactor day10

--- crates/day10/src/main.rs
+++ crates/day10/src/main.rs
@@ -1,4 +1,4 @@
-use grid::Grid;
+use grid::{Grid, Position};
 use pathfinding::prelude::{astar, astar_bag};
 
 use utils::*;
@@ -6,28 +6,53 @@ use utils::*;
 const TEST: &str = include_str!("./input/test.txt");
 const INPUT: &str = include_str!("./input/input.txt");
 
+struct HeightMap {
+    grid: Grid<usize>,
+    trail_heads: Vec<Position>,
+    peaks: Vec<Position>,
+}
+
+impl HeightMap {
+    fn from_str(input: &str) -> Self {
+        let grid = Grid::from_str(input, |c| c.to_digit(10).unwrap() as usize);
+        let trail_heads = grid.get_positions_where(|v| *v == 0);
+        let peaks = grid.get_positions_where(|v| *v == 9);
+        Self {
+            grid,
+            trail_heads,
+            peaks,
+        }
+    }
+
+    fn neighbors(&self, pos: &Position) -> Vec<(Position, usize)> {
+        self.grid
+            .neighbors_cardinal(*pos)
+            .iter()
+            .filter(|neighbor_pos| {
+                let curr_height = self.grid.get(*pos).unwrap();
+                let next_height = *self.grid.get(**neighbor_pos).unwrap();
+                next_height == curr_height + 1
+            })
+            .map(|pos| (*pos, 1))
+            .collect::<Vec<_>>()
+    }
+
+    fn heuristic(&self, pos: Position, peak: Position) -> usize {
+        self.grid.distance_cardinal(pos, peak)
+    }
+}
+
 fn part1(input: &str) -> usize {
-    let grid = Grid::from_str(input, |c| c.to_digit(10).unwrap() as usize);
-    let trail_heads = grid.get_positions_where(|v| *v == 0);
-    let peaks = grid.get_positions_where(|v| *v == 9);
-    trail_heads.iter().fold(0, |sum, trail_head| {
-        sum + peaks
+    let map = HeightMap::from_str(input);
+    map.trail_heads.iter().fold(0, |sum, trail_head| {
+        sum + map
+            .peaks
             .iter()
             .filter(|peak| {
                 astar(
                     trail_head,
-                    |curr_pos| {
-                        grid.neighbors_cardinal(*curr_pos)
-                            .iter()
-                            .filter(|neighbor_pos| {
-                                let curr_height = grid.get(*curr_pos).unwrap();
-                                let next_height = *grid.get(**neighbor_pos).unwrap();
-                                next_height == curr_height + 1
-                            })
-                            .map(|pos| (*pos, 1))
-                            .collect::<Vec<_>>()
-                    },
-                    |pos| grid.distance_cardinal(*pos, **peak),
+                    |curr_pos| map.neighbors(curr_pos),
+                    |pos| map.heuristic(*pos, **peak),
                     |pos| pos == *peak,
                 )
                 .is_some()
@@ -37,32 +62,18 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    let grid = Grid::from_str(input, |c| c.to_digit(10).unwrap() as usize);
-    let trail_heads = grid.get_positions_where(|v| *v == 0);
-    let peaks = grid.get_positions_where(|v| *v == 9);
-    trail_heads.iter().fold(0, |sum, trail_head| {
-        sum + peaks
-            .iter()
-            .filter_map(|peak| {
-                astar_bag(
-                    trail_head,
-                    |curr_pos| {
-                        grid.neighbors_cardinal(*curr_pos)
-                            .iter()
-                            .filter(|neighbor_pos| {
-                                let curr_height = grid.get(*curr_pos).unwrap();
-                                let next_height = *grid.get(**neighbor_pos).unwrap();
-                                next_height == curr_height + 1
-                            })
-                            .map(|pos| (*pos, 1))
-                            .collect::<Vec<_>>()
-                    },
-                    |pos| grid.distance_cardinal(*pos, *peak),
-                    |pos| pos == peak,
-                )
-                .map(|(solution, _)| solution.count())
-            })
-            .sum::<usize>()
+    let map = HeightMap::from_str(input);
+    map.trail_heads.iter().fold(0, |sum, trail_head| {
+        sum + map.peaks.iter().fold(0, |sum, peak| {
+            sum + astar_bag(
+                trail_head,
+                |curr_pos| map.neighbors(curr_pos),
+                |pos| map.heuristic(*pos, *peak),
+                |pos| pos == peak,
+            )
+            .map(|(solution, _)| solution.count())
+            .unwrap_or(0)
+        })
     })
 }
 
