02d6c30b8c3291a0a9adb82b5521faa1ca7257bb
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2024-12-10 10:32:19 +0100

    day10 part1

--- crates/day10/src/main.rs
+++ crates/day10/src/main.rs
@@ -1,6 +1,5 @@
-use grid::{Grid, Position};
+use grid::Grid;
 use pathfinding::prelude::astar;
-use std::collections::HashSet;
 
 use utils::*;
 
@@ -9,46 +8,32 @@ const INPUT: &str = include_str!("./input/input.txt");
 
 fn part1(input: &str) -> usize {
     let grid = Grid::from_str(input, |c| c.to_digit(10).unwrap() as usize);
-    let starting_positions = grid.get_positions_where(|v| *v == 0);
-    let ending_positions = grid.get_positions_where(|v| *v == 9);
-    starting_positions
-        .iter()
-        .fold(
-            HashSet::<(Position, usize)>::new(),
-            |mut successes, start| {
-                let search_in = ending_positions
-                    .iter()
-                    .filter(|end| !successes.iter().any(|(success_pos, _)| success_pos == *end))
-                    .map(|x| *x)
-                    .collect::<Vec<_>>();
-                if search_in.is_empty() {
-                    return successes;
-                }
-                successes.extend(search_in.iter().filter_map(|end| {
-                    if let Some((_, score)) = astar(
-                        start,
-                        |a| {
-                            grid.neighbors_cardinal(*a)
-                                .iter()
-                                .filter(|b| {
-                                    grid.get(*a).unwrap().abs_diff(*grid.get(**b).unwrap()) == 1
-                                })
-                                .map(|a| (*a, 1))
-                                .collect::<Vec<_>>()
-                        },
-                        |a| grid.distance_cardinal(*a, *end),
-                        |x| *x == *end,
-                    ) {
-                        Some((*end, score))
-                    } else {
-                        None
-                    }
-                }));
-                successes
-            },
-        )
-        .iter()
-        .fold(0, |acc, (_, score)| acc + score)
+    let trail_heads = grid.get_positions_where(|v| *v == 0);
+    let peaks = grid.get_positions_where(|v| *v == 9);
+    trail_heads.iter().fold(0, |sum, trail_head| {
+        sum + peaks
+            .iter()
+            .filter(|peak| {
+                astar(
+                    trail_head,
+                    |curr_pos| {
+                        grid.neighbors_cardinal(*curr_pos)
+                            .iter()
+                            .filter(|neighbor_pos| {
+                                let curr_height = grid.get(*curr_pos).unwrap();
+                                let next_height = *grid.get(**neighbor_pos).unwrap();
+                                next_height == curr_height + 1
+                            })
+                            .map(|pos| (*pos, 1))
+                            .collect::<Vec<_>>()
+                    },
+                    |pos| grid.distance_cardinal(*pos, **peak),
+                    |pos| pos == *peak,
+                )
+                .is_some()
+            })
+            .count()
+    })
 }
 
 fn part2(input: &str) -> usize {
@@ -57,7 +42,7 @@ fn part2(input: &str) -> usize {
 
 fn main() {
     test_part1(|| part1(TEST), 36);
-    // answer_part1(|| part1(INPUT), 0);
+    answer_part1(|| part1(INPUT), 510);
     // test_part2(|| part2(TEST), 0);
     // answer_part2(|| part2(INPUT), 0);
 }
