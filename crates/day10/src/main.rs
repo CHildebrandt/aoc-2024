ed685094a1a6ea070a6bba1dacec5f396b22e765
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2024-12-10 10:41:34 +0100

    day10 part2

--- crates/day10/src/main.rs
+++ crates/day10/src/main.rs
@@ -1,5 +1,5 @@
 use grid::Grid;
-use pathfinding::prelude::astar;
+use pathfinding::prelude::{astar, astar_bag};
 
 use utils::*;
 
@@ -37,12 +37,38 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    0
+    let grid = Grid::from_str(input, |c| c.to_digit(10).unwrap() as usize);
+    let trail_heads = grid.get_positions_where(|v| *v == 0);
+    let peaks = grid.get_positions_where(|v| *v == 9);
+    trail_heads.iter().fold(0, |sum, trail_head| {
+        sum + peaks
+            .iter()
+            .filter_map(|peak| {
+                astar_bag(
+                    trail_head,
+                    |curr_pos| {
+                        grid.neighbors_cardinal(*curr_pos)
+                            .iter()
+                            .filter(|neighbor_pos| {
+                                let curr_height = grid.get(*curr_pos).unwrap();
+                                let next_height = *grid.get(**neighbor_pos).unwrap();
+                                next_height == curr_height + 1
+                            })
+                            .map(|pos| (*pos, 1))
+                            .collect::<Vec<_>>()
+                    },
+                    |pos| grid.distance_cardinal(*pos, *peak),
+                    |pos| pos == peak,
+                )
+                .map(|(solution, _)| solution.count())
+            })
+            .sum::<usize>()
+    })
 }
 
 fn main() {
     test_part1(|| part1(TEST), 36);
     answer_part1(|| part1(INPUT), 510);
-    // test_part2(|| part2(TEST), 0);
-    // answer_part2(|| part2(INPUT), 0);
+    test_part2(|| part2(TEST), 81);
+    answer_part2(|| part2(INPUT), 1058);
 }
