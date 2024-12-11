a53ac8f7befce12a88872d07912e75ed5f5cab8d
Christoffer Hildebrandt <christoffer.hildebrandt@gmail.com>
2024-12-11 22:04:46 +0100

    add macros

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -23,3 +23,31 @@ pub fn test_part2(get: impl FnOnce() -> usize, actual: usize) {
 pub fn answer_part2(get: impl FnOnce() -> usize, actual: usize) {
     answer(get, actual, false, 2);
 }
+
+#[macro_export]
+macro_rules! part1_test {
+    ($a:expr, $b:expr) => {{
+        utils::test_part1(|| part1(&$a.trim()), $b);
+    }};
+}
+
+#[macro_export]
+macro_rules! part1_answer {
+    ($a:expr, $b:expr) => {{
+        utils::answer_part1(|| part1(&$a.trim()), $b);
+    }};
+}
+
+#[macro_export]
+macro_rules! part2_test {
+    ($a:expr, $b:expr) => {{
+        utils::test_part2(|| part1(&$a.trim()), $b);
+    }};
+}
+
+#[macro_export]
+macro_rules! part2_answer {
+    ($a:expr, $b:expr) => {{
+        utils::answer_part2(|| part1(&$a.trim()), $b);
+    }};
+}
