88b984ba31e6be1db690fc78b40074b3ff5c2f6b
Christoffer Hildebrandt <christoffer.hildebrandt@gmail.com>
2024-12-16 19:05:49 +0100

    day16 refactor

--- crates/day16/src/main.rs
+++ crates/day16/src/main.rs
@@ -1,55 +1,13 @@
-use std::collections::HashSet;
-
 use direction::CardinalDirection;
-use grid::Grid;
+use grid::{Grid, Position};
 use itertools::Itertools;
-use pathfinding::prelude::{astar, astar_bag};
+use pathfinding::prelude::{astar_bag, AstarSolution};
 use utils::*;
 
-fn part1(input: &str) -> usize {
+fn parse(input: &str) -> (AstarSolution<(Position, CardinalDirection)>, usize) {
     let grid = Grid::char_grid(input);
     let start = grid.find(|c| c == &'S').unwrap();
     let end = grid.find(|c| c == &'E').unwrap();
-    astar(
-        &(start, CardinalDirection::East),
-        |(curr, facing)| {
-            grid.neighbors_cardinal(*curr)
-                .iter()
-                .filter_map(|next| {
-                    if grid.get(*next).unwrap() == &'#' {
-                        None
-                    } else {
-                        let diff = (
-                            next.0 as isize - curr.0 as isize,
-                            next.1 as isize - curr.1 as isize,
-                        );
-                        let next_facing = CardinalDirection::from_diff(diff).unwrap();
-                        if facing.is_180(&next_facing) {
-                            None
-                        } else {
-                            let mut cost = 1;
-                            if facing != &next_facing {
-                                cost += 1000;
-                            }
-                            Some(((*next, next_facing), cost))
-                        }
-                    }
-                })
-                .collect_vec()
-        },
-        |(pos, _)| grid.distance_cardinal(*pos, end),
-        |(pos, _)| pos == &end,
-    )
-    .unwrap()
-    .1
-}
-
-fn part2(input: &str) -> usize {
-    let grid = Grid::char_grid(input);
-    let start = grid.find(|c| c == &'S').unwrap();
-    let end = grid.find(|c| c == &'E').unwrap();
-    let mut best_seats = HashSet::new();
-    best_seats.extend(vec![start, end]);
     astar_bag(
         &(start, CardinalDirection::East),
         |(curr, facing)| {
@@ -67,10 +25,7 @@ fn part2(input: &str) -> usize {
                         if facing.is_180(&next_facing) {
                             None
                         } else {
-                            let mut cost = 1;
-                            if facing != &next_facing {
-                                cost += 1000;
-                            }
+                            let cost = if facing != &next_facing { 1001 } else { 1 };
                             Some(((*next, next_facing), cost))
                         }
                     }
@@ -81,13 +36,18 @@ fn part2(input: &str) -> usize {
         |(pos, _)| pos == &end,
     )
     .unwrap()
-    .0
-    .for_each(|steps| {
-        steps.iter().for_each(|(pos, _)| {
-            best_seats.insert(*pos);
-        });
-    });
-    best_seats.len()
+}
+
+fn part1(input: &str) -> usize {
+    parse(input).1
+}
+
+fn part2(input: &str) -> usize {
+    parse(input)
+        .0
+        .flat_map(|steps| steps.iter().map(|(pos, _)| *pos).collect_vec())
+        .unique()
+        .count()
 }
 
 fn main() {
