ff689a7fdb368e720d03150b59e6e198f3fc4fdd
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2024-12-22 19:54:20 +0100

    day22 part2

--- crates/day22/src/main.rs
+++ crates/day22/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use itertools::Itertools;
 use utils::*;
 
@@ -28,52 +30,51 @@ fn part1(input: &str) -> usize {
 }
 
 fn part2(input: &str) -> usize {
-    // let mut secret_numbers = input
-    //     .lines()
-    //     .map(|line| line.parse::<usize>().unwrap())
-    //     .collect_vec();
-    let mut secret_numbers = vec![123];
-    let mut changes: Vec<Vec<(usize, Option<isize>)>> = vec![vec![]; secret_numbers.len()];
-    for _ in 0..10 {
-        for (i, (num, changes)) in secret_numbers
-            .iter_mut()
-            .zip(changes.iter_mut())
-            .enumerate()
-        {
-            let before = *num;
-            let last_digit_before = extract_last_digit(before);
+    let mut secret_numbers = input
+        .lines()
+        .map(|line| line.parse::<usize>().unwrap())
+        .collect_vec();
+    let mut price_changes: Vec<Vec<(usize, Option<isize>)>> = vec![vec![]; secret_numbers.len()];
+    for _ in 0..2000 {
+        for (num, changes) in secret_numbers.iter_mut().zip(price_changes.iter_mut()) {
+            let last_digit_before = extract_last_digit(*num);
             update_secret(num);
-            let last_digit_after = extract_last_digit(before);
             let change = if changes.is_empty() {
                 None
             } else {
-                Some(last_digit_after as isize - changes.last().unwrap().0 as isize)
+                Some(last_digit_before as isize - changes.last().unwrap().0 as isize)
             };
-            println!("{}: {} ({:?})", before, last_digit_before, change);
-            changes.push((last_digit_after, change));
+            changes.push((last_digit_before, change));
         }
     }
-    changes
+    let mut map = HashMap::<(isize, isize, isize, isize), Vec<usize>>::new();
+    price_changes.iter().for_each(|changes| {
+        changes
+            .iter()
+            .skip(1)
+            .map(|x| (x.0, x.1.unwrap()))
+            .tuple_windows()
+            .filter(|(a, b, c, d)| a.1 != b.1 && b.1 != c.1 && c.1 != d.1)
+            .map(|(a, b, c, d)| ((a.1, b.1, c.1, d.1), d.0))
+            .unique_by(|&(sequence, _)| sequence)
+            .for_each(|(sequence, score)| {
+                if !map.contains_key(&sequence) {
+                    map.insert(sequence, vec![]);
+                }
+                map.get_mut(&sequence).unwrap().push(score);
+            });
+    });
+    map.iter()
+        .max_by(|(_, a), (_, b)| a.iter().sum::<usize>().cmp(&b.iter().sum::<usize>()))
+        .unwrap()
+        .1
         .iter()
-        .map(|changes| {
-            let sequences = changes
-                .iter()
-                .skip(1)
-                .map(|x| (x.0, x.1.unwrap()))
-                .take(10)
-                .tuple_windows()
-                .filter(|(a, b, c, d)| a.1 != b.1 && b.1 != c.1 && c.1 != d.1)
-                .collect_vec();
-            changes
-        })
-        .collect_vec();
-    0
+        .sum()
 }
 
 fn main() {
     part1_test!(37327623);
     part1_answer!(18317943467);
-    // test_part2(|| part2(include_str!("./input/test2.txt")), 23);
-    // part2_test!(0);
-    // part2_answer!(0);
+    test_part2(|| part2(include_str!("./input/test2.txt")), 23);
+    part2_answer!(2018);
 }
