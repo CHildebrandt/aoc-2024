9c0815a00d37370c9bedc321ae9c75b19aeb8d1b
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2024-12-24 00:36:38 +0100

    day23 part2

--- crates/day23/src/main.rs
+++ crates/day23/src/main.rs
@@ -1,25 +1,30 @@
 use std::collections::{HashMap, HashSet};
 
 use itertools::Itertools;
+use std::iter::once;
 use utils::*;
 
-fn part1(input: &str) -> usize {
-    let all_sorted = input
+fn parse(input: &str) -> HashMap<&str, HashSet<&str>> {
+    let mut map = HashMap::<&str, HashSet<&str>>::new();
+    input
         .lines()
         .map(|line| line.split_once("-").unwrap())
         .unique()
-        .collect_vec();
-    let mut map = HashMap::<&str, HashSet<&str>>::new();
-    all_sorted.iter().for_each(|(key, val)| {
-        map.entry(*key).or_default().insert(*val);
-        map.entry(*val).or_default().insert(*key);
-    });
+        .for_each(|(key, val)| {
+            map.entry(key).or_default().insert(val);
+            map.entry(val).or_default().insert(key);
+        });
+    map
+}
+
+fn part1(input: &str) -> usize {
+    let map = parse(input);
     map.iter()
         .filter(|(key, _)| key.starts_with("t"))
         .flat_map(|(a, set)| {
             set.iter()
                 .flat_map(|b| {
-                    map.get(*b)
+                    map.get(b)
                         .unwrap()
                         .intersection(set)
                         .map(|c| {
@@ -35,13 +40,31 @@ fn part1(input: &str) -> usize {
         .count()
 }
 
-fn part2(input: &str) -> usize {
-    0
+fn part2(input: &str) -> String {
+    parse(input)
+        .iter()
+        // Unfortunately, sorting is needed. Might not work with other inputs...
+        .sorted_by_key(|(key, _)| *key)
+        .fold(vec![], |mut sets: Vec<HashSet<&str>>, (key, set)| {
+            if let Some(set) = sets.iter_mut().find(|curr| curr.is_subset(set)) {
+                set.insert(*key);
+            } else {
+                sets.push(HashSet::from_iter(once(*key)));
+            }
+            sets
+        })
+        .iter()
+        .map(|set| set.iter().sorted().collect_vec())
+        .max_by_key(Vec::len)
+        .unwrap()
+        .iter()
+        .sorted()
+        .join(",")
 }
 
 fn main() {
     part1_test!(7);
     part1_answer!(1411);
-    // part2_test!(0);
-    // part2_answer!(0);
+    part2_test!("co,de,ka,ta".into());
+    part2_answer!("aq,bn,ch,dt,gu,ow,pk,qy,tv,us,yx,zg,zu".into());
 }
