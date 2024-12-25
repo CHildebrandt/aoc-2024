f594493d54dc5177a54905cd3ebdf5f15c342497
Christoffer Valentin Hildebrandt <Christoffer.Hildebrandt@pdc.com>
2024-12-25 18:41:47 +0100

    day24 part2

--- crates/day24/src/main.rs
+++ crates/day24/src/main.rs
@@ -66,6 +66,18 @@ impl From<&str> for Gate {
     }
 }
 
+impl Gate {
+    fn has_input(&self, name: &str) -> bool {
+        self.input_a == name || self.input_b == name
+    }
+
+    fn any_connection_starts_with(&self, name: &[char]) -> bool {
+        self.input_a.starts_with(name)
+            || self.input_b.starts_with(name)
+            || self.output.starts_with(name)
+    }
+}
+
 fn part1(input: &str) -> usize {
     let (wires, gates) = split_double_newline_once(input);
     let mut wires = wires
@@ -77,7 +89,7 @@ fn part1(input: &str) -> usize {
     let z_gates = gates
         .iter()
         .filter_map(|gate| gate.output.starts_with("z").then_some(gate.output.clone()))
-        .collect::<Vec<_>>();
+        .collect_vec();
     loop {
         for gate in &gates {
             if let Some((&a, &b)) = wires.get(&gate.input_a).zip(wires.get(&gate.input_b)) {
@@ -98,14 +110,45 @@ fn part1(input: &str) -> usize {
     usize::from_str_radix(&bin, 2).unwrap()
 }
 
-fn part2(input: &str) -> usize {
-    0
+fn part2(input: &str) -> String {
+    let (_, gates) = split_double_newline_once(input);
+    let gates = gates.lines().map(Gate::from).collect_vec();
+    let max_z = gates
+        .iter()
+        .filter_map(|gate| gate.output.starts_with("z").then_some(gate.output.clone()))
+        .max()
+        .unwrap();
+    gates
+        .iter()
+        .filter(|gate| {
+            if gate.output.starts_with('z') && gate.output != max_z && gate.logic != Logic::XOr {
+                true
+            } else {
+                match gate.logic {
+                    Logic::And => {
+                        !gate.has_input("x00")
+                            && gates.iter().any(|checked| {
+                                checked.has_input(&gate.output) && checked.logic != Logic::Or
+                            })
+                    }
+                    Logic::Or => false,
+                    Logic::XOr => {
+                        !gate.any_connection_starts_with(&['x', 'y', 'z'])
+                            || gates.iter().any(|checked| {
+                                checked.has_input(&gate.output) && checked.logic == Logic::Or
+                            })
+                    }
+                }
+            }
+        })
+        .map(|gate| gate.output.clone())
+        .sorted()
+        .join(",")
 }
 
 fn main() {
     part1_test!(4);
     test_part1(|| part1(include_str!("./input/test2.txt")), 2024);
     part1_answer!(55114892239566);
-    // part2_test!(0);
-    // part2_answer!(0);
+    part2_answer!("cdj,dhm,gfm,mrb,qjd,z08,z16,z32".into());
 }
